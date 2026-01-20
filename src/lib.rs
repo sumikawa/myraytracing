589679f3701d4e600df87c3534d88cda23c04b81
Munechika Sumikawa <sumikawa@sumikawa.jp>
2026-01-21 01:48:59 +0900

    List 1.8: Add Ray module

--- src/lib.rs
+++ src/lib.rs
@@ -160,3 +160,26 @@ impl DivAssign<f64> for Vec3 {
         *self *= 1.0 / rhs;
     }
 }
+
+pub struct Ray {
+    pub origin: Point3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Self { origin, direction }
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.origin
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.direction
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.origin + self.direction * t
+    }
+}
