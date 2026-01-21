9af7776e68d5569f83694c190e8d7ec738fb071f
Munechika Sumikawa <sumikawa@sumikawa.jp>
2026-01-21 17:55:09 +0900

    Split lib.rs add refactoring codes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use myraytracing::*;
+use myraytracing::ray::Ray;
+use myraytracing::vec3::{Color, Point3, Vec3};
 
 fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc: Vec3 = r.origin() - center;
@@ -6,25 +7,25 @@ fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let a: f64 = di.dot(di);
     let b: f64 = 2.0 * di.dot(oc);
     let c: f64 = oc.dot(oc) - radius * radius;
-    let discriminant: f64 = b * b - 4.0 * a *c;
+    let discriminant: f64 = b * b - 4.0 * a * c;
 
     if discriminant < 0.0 {
-	return -1.0;
+        -1.0
     } else {
-	return ( -b - discriminant.sqrt() ) / (2.0 * a);
+        (-b - discriminant.sqrt()) / (2.0 * a)
     }
 }
 
 fn ray_color(r: Ray) -> Vec3 {
     let t: f64 = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r);
     if t > 0.0 {
-	let n: Vec3 = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
-	return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
+        let n: Vec3 = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
 
     let unit_direction: Vec3 = r.direction().unit_vector();
-    let t: f64 = 0.5 * (unit_direction.y + 1.0);
-    return (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0);
+    let t: f64 = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
 fn write_color(pixel_color: Color) {
@@ -52,14 +53,17 @@ fn main() {
     let horizontal: Vec3 = Vec3::new(viewport_width, 0.0, 0.0);
     let vertical: Vec3 = Vec3::new(0.0, viewport_height, 0.0);
     let lower_left_corner: Vec3 =
-	origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("\rScanlines remaining: {} ", j);
         for i in 0..IMAGE_WIDTH {
-	    let u: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
-	    let v: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-	    let r: Ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            let u: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let r: Ray = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
 
             let pixel_color = ray_color(r);
             write_color(pixel_color);
---
+++ src/vec3.rs
@@ -0,0 +1,183 @@
+use std::fmt;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub};
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+pub type Point3 = Vec3;
+pub type Color = Vec3;
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+
+    pub fn z(&self) -> f64 {
+        self.z
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn dot(&self, other: Self) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn cross(&self, other: Self) -> Self {
+        Self {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    pub fn unit_vector(&self) -> Self {
+        *self / self.length()
+    }
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.x, self.y, self.z)
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        };
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        *self = Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        };
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        *self *= 1.0 / rhs;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(4.0, 5.0, 6.0);
+        let result = v1 + v2;
+        assert_eq!(result, Vec3::new(5.0, 7.0, 9.0));
+    }
+
+    #[test]
+    fn test_unit_vector() {
+        let a = Vec3::new(1.0, 2.0, 3.0);
+        let unit_direction = a.unit_vector();
+        // A simple check to see if the length is close to 1.0
+        assert!((unit_direction.length() - 1.0).abs() < 0.000001);
+    }
+}
