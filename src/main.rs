d3b6909ba64e81a8c58e21a0432638b1df7995a7
Munechika Sumikawa <sumikawa@sumikawa.jp>
2026-01-21 14:05:26 +0900

    List 1.9: Rendering blue-white gradation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use myraytracing::Color;
+use myraytracing::*;
+
+fn ray_color(r: Ray) -> Vec3 {
+    let unit_direction: Vec3 = r.direction().unit_vector();
+    let t: f64 = 0.5 * (unit_direction.y + 1.0);
+    return (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0);
+}
 
 fn write_color(pixel_color: Color) {
     // Write the translated [0,255] value of each color component.
@@ -11,19 +17,30 @@ fn write_color(pixel_color: Color) {
 }
 
 fn main() {
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i32 = 384;
+    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
 
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    let viewport_height: f64 = 2.0;
+    let viewport_width: f64 = ASPECT_RATIO * viewport_height;
+    let focal_length: f64 = 1.0;
+
+    let origin: Point3 = Point3::new(0.0, 0.0, 0.0);
+    let horizontal: Vec3 = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical: Vec3 = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner: Vec3 =
+	origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("\rScanlines remaining: {} ", j);
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new(
-                i as f64 / (IMAGE_WIDTH - 1) as f64,
-                j as f64 / (IMAGE_HEIGHT - 1) as f64,
-                0.25,
-            );
+	    let u: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
+	    let v: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+	    let r: Ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+
+            let pixel_color = ray_color(r);
             write_color(pixel_color);
         }
     }
